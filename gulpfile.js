4f5c4433a5e780ec57079ed5c78af6b15c9d6324
Levi Durfee <levi.durfee@gmail.com>
2017-06-13 19:18:53 -0400

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+gulp = require('gulp');
+babel = require('gulp-babel');
+eslint = require('gulp-eslint');
+concat = require('gulp-concat');
+minify = require('gulp-minify');
+sass = require('gulp-sass');
+csscomb = require('gulp-csscomb');
+cleanCSS = require('gulp-clean-css');
+autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('scripts:transpile', function() {
+    return gulp.src([
+        //
+    ])
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(concat('jabberwocky.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('scripts:minify', function() {
+    return gulp.src('./build/jabberwocky.js')
+    .pipe(minify({
+        ext: { min: 'min.js' }
+    }))
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('css:scss', function() {
+    return gulp.src('./src/scss/*.scss')
+    .pipe(sass()
+        .on('error', sass.logError))
+    .pipe(csscomb())
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions'],
+        cascade: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(concat('jabberwocky.css'))
+    .pipe(gulp.dest('./src/css'));
+});
