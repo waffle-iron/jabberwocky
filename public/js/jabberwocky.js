25481f29837b6527b88c13c57f485ba09b67a378
Levi Durfee <levi.durfee@gmail.com>
2017-06-21 19:37:32 -0400

    add auth listener

--- public/js/jabberwocky.js
+++ public/js/jabberwocky.js
@@ -190,20 +190,27 @@ var Message = function () {
 var template = new Template();
 var user = new User();
 var message = new Message();
-// template 
-//   .empty('main') 
-//   .load('login', 'main') 
-//   .listen('login', user, 'login'); 
-// template.load('register', 'main');
-template.empty('main').load('chat', 'main');
-template.listen('chat', message, 'send');
+
+template.empty('main').load('login', 'main').listen('login', user, 'login');
+template.load('register', 'main');
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    template.empty('main').load('chat', 'main');
+    template.listen('chat', message, 'send');
+  }
+});
 
 var msgsRef = firebase.database().ref('messages/');
 msgsRef.on('child_added', function (data) {
   message.update('chat__messages', data.val());
 });
 
-// let msgsRef = firebase.database().ref('messages/');
-// msgsRef.on('child_added', function(data) {
-//   message.update('chat__messages', data.val());
-// });
+/*
+var self = this;
+    return this.database.ref('messages/').push({
+      'date': Date(),
+      'user': from,
+      'message': self.aes.encrypt(message, from)
+    }).key;
+ */
--- src/js/jabberwocky.js
+++ src/js/jabberwocky.js
@@ -1,20 +1,32 @@
 const template = new Template;
 const user = new User;
 const message = new Message;
-// template 
-//   .empty('main') 
-//   .load('login', 'main') 
-//   .listen('login', user, 'login'); 
-// template.load('register', 'main');
-template.empty('main').load('chat', 'main');
-template.listen('chat', message, 'send');
+
+template 
+  .empty('main') 
+  .load('login', 'main') 
+  .listen('login', user, 'login'); 
+template.load('register', 'main');
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    template.empty('main').load('chat', 'main');
+    template.listen('chat', message, 'send');
+  }
+});
+
+
 
 let msgsRef = firebase.database().ref('messages/');
 msgsRef.on('child_added', function(data) {
   message.update('chat__messages', data.val());
 });
 
-// let msgsRef = firebase.database().ref('messages/');
-// msgsRef.on('child_added', function(data) {
-//   message.update('chat__messages', data.val());
-// });
+/*
+var self = this;
+    return this.database.ref('messages/').push({
+      'date': Date(),
+      'user': from,
+      'message': self.aes.encrypt(message, from)
+    }).key;
+ */
